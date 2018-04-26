ae08539ed303896bbd542127e74263eda029b378
伊藤 和宏 <ito_kazuhiro@cyberagent.co.jp>
2018-04-26 10:42:24 +0900

    avroファイル入力、attribute phで年月日時のディレクトリ振り分け

--- src/main/java/org/apache/beam/examples/complete/game/TestPubsubToGcs.java
+++ src/main/java/org/apache/beam/examples/complete/game/TestPubsubToGcs.java
@@ -1,28 +1,16 @@
 package org.apache.beam.examples.complete.game;
 
-import java.io.File;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.nio.file.Files;
 import java.util.Locale;
-import java.util.Map;
 
 import org.apache.avro.Schema;
-import org.apache.avro.generic.GenericRecord;
-import org.apache.beam.examples.complete.game.PubsubToGcs.ParseTrackingAdFn;
-import org.apache.beam.examples.complete.game.PubsubToGcs.ReadDataOptions;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.coders.AvroCoder;
 import org.apache.beam.sdk.io.AvroIO;
-import org.apache.beam.sdk.io.DefaultFilenamePolicy;
 import org.apache.beam.sdk.io.DynamicAvroDestinations;
 import org.apache.beam.sdk.io.FileBasedSink;
-import org.apache.beam.sdk.io.FileBasedSink.DynamicDestinations;
 import org.apache.beam.sdk.io.FileBasedSink.FilenamePolicy;
 import org.apache.beam.sdk.io.FileSystems;
-import org.apache.beam.sdk.io.TextIO;
-import org.apache.beam.sdk.io.fs.ResolveOptions;
-import org.apache.beam.sdk.io.fs.ResourceId;
 import org.apache.beam.sdk.io.gcp.pubsub.PubsubIO;
 import org.apache.beam.sdk.io.gcp.pubsub.PubsubMessage;
 import org.apache.beam.sdk.options.Default;
@@ -30,33 +18,19 @@ import org.apache.beam.sdk.options.Description;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.options.Validation;
-import org.apache.beam.sdk.options.ValueProvider;
-import org.apache.beam.sdk.options.ValueProvider.NestedValueProvider;
 import org.apache.beam.sdk.options.ValueProvider.StaticValueProvider;
-import org.apache.beam.sdk.transforms.Count;
-import org.apache.beam.sdk.transforms.Create;
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.ParDo;
-import org.apache.beam.sdk.transforms.SerializableFunction;
-import org.apache.beam.sdk.transforms.DoFn.ProcessContext;
-import org.apache.beam.sdk.transforms.DoFn.ProcessElement;
 import org.apache.beam.sdk.transforms.windowing.FixedWindows;
-import org.apache.beam.sdk.transforms.windowing.SlidingWindows;
 import org.apache.beam.sdk.transforms.windowing.Window;
 import org.apache.beam.sdk.util.CoderUtils;
-import org.apache.beam.sdk.values.KV;
 import org.apache.beam.sdk.values.PCollection;
-import org.apache.beam.sdk.values.PCollectionView;
 import org.joda.time.DateTime;
 import org.joda.time.Duration;
 import org.joda.time.MutablePeriod;
 import org.joda.time.format.PeriodFormatterBuilder;
 import org.joda.time.format.PeriodParser;
-
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.base.Joiner;
 import com.google.common.base.Preconditions;
 
 public class TestPubsubToGcs {
@@ -86,30 +60,36 @@ public class TestPubsubToGcs {
         Integer getNumShards();
         void setNumShards(Integer value);
     }
+    
+    static class ParseTrackingAdFn extends DoFn<PubsubMessage, TrackingAdBQ> {
+        /**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
 
- /*   
-    static class ParseTrackingAdFn extends DoFn<PubsubMessage, PubsubMessage> {
-        @ProcessElement
+		@ProcessElement
         public void processElement(ProcessContext c) throws IOException {
             // Input(json) -> TrackingAd
             try {
-            	
             	PubsubMessage pubsubMessage = c.element();
-                Map<String,String> map = pubsubMessage.getAttributeMap();
-                String attr = pubsubMessage.getAttribute("ph");
-                String json = new String(pubsubMessage.getPayload(), "UTF-8");
-                System.out.println("attribute:" + attr);
+ //           	String json = new String(pubsubMessage.getPayload(), "UTF-8");
+  //              System.out.println("formatRecord:json:" + json);
+  //              ObjectMapper mapper = new ObjectMapper();
+  //              TrackingAd trackingAd = mapper.readValue(json, TrackingAd.class);
+   
+                AvroCoder<TrackingAd> coder = AvroCoder.of(TrackingAd.class);
+                TrackingAd trackingAd = CoderUtils.decodeFromByteArray(coder, pubsubMessage.getPayload());
+                TrackingAdBQ trackingAdbq = TrackingAdMap.TrackingAdToTrackingAdBQ(trackingAd);
 
-                ObjectMapper mapper = new ObjectMapper();
-                TrackingAd trackingAd = mapper.readValue(json, TrackingAd.class);
-                c.output(pubsubMessage);
+                c.output(trackingAdbq);
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
-*/
-    private static final String SCHEMA_STRING =
+
+    private static final String SCHEMA_STRING = "{\"type\":\"record\",\"name\":\"TrackingAdBQ\",\"namespace\":\"org.apache.beam.examples.complete.game\",\"fields\":[{\"name\":\"uuid\",\"type\":[\"null\",\"string\"],\"default\":null},{\"name\":\"time\",\"type\":[\"null\",\"string\"],\"default\":null},{\"name\":\"user_id\",\"type\":[\"null\",\"string\"],\"default\":null},{\"name\":\"client_ad_id\",\"type\":[\"null\",\"string\"],\"default\":null},{\"name\":\"os\",\"type\":[\"null\",\"string\"],\"default\":null},{\"name\":\"ip\",\"type\":[\"null\",\"string\"],\"default\":null},{\"name\":\"log_type\",\"type\":[\"null\",\"int\"],\"default\":null},{\"name\":\"tracking_timing\",\"type\":[\"null\",\"int\"],\"default\":null},{\"name\":\"cue_point_id\",\"type\":[\"null\",\"long\"],\"default\":null},{\"name\":\"cluster_cf_id\",\"type\":[\"null\",\"int\"],\"default\":null},{\"name\":\"cluster_id\",\"type\":[\"null\",\"int\"],\"default\":null},{\"name\":\"segment_group_id\",\"type\":[\"null\",\"string\"],\"default\":null},{\"name\":\"cue_point_sequence\",\"type\":[\"null\",\"int\"],\"default\":null},{\"name\":\"inserted_second\",\"type\":[\"null\",\"int\"],\"default\":null},{\"name\":\"inserted_start_datetime\",\"type\":[\"null\",\"long\"],\"default\":null},{\"name\":\"inserted_end_datetime\",\"type\":[\"null\",\"long\"],\"default\":null},{\"name\":\"slot_id\",\"type\":[\"null\",\"string\"],\"default\":null},{\"name\":\"channel_id\",\"type\":[\"null\",\"string\"],\"default\":null},{\"name\":\"program_id\",\"type\":[\"null\",\"string\"],\"default\":null},{\"name\":\"content_provider_id\",\"type\":[\"null\",\"string\"],\"default\":null},{\"name\":\"placement_method\",\"type\":[\"null\",\"long\"],\"default\":null},{\"name\":\"placement_job_id\",\"type\":[\"null\",\"string\"],\"default\":null},{\"name\":\"token_id\",\"type\":[\"null\",\"string\"],\"default\":null},{\"name\":\"account_id\",\"type\":[\"null\",\"long\"],\"default\":null},{\"name\":\"account_type\",\"type\":[\"null\",\"int\"],\"default\":null},{\"name\":\"campaign_id\",\"type\":[\"null\",\"long\"],\"default\":null},{\"name\":\"ad_id\",\"type\":[\"null\",\"long\"],\"default\":null},{\"name\":\"creative_id\",\"type\":[\"null\",\"long\"],\"default\":null},{\"name\":\"creative_asset_id\",\"type\":[\"null\",\"long\"],\"default\":null},{\"name\":\"agency_id\",\"type\":[\"null\",\"long\"],\"default\":null},{\"name\":\"advertiser_brand_id\",\"type\":[\"null\",\"long\"],\"default\":null},{\"name\":\"business_id\",\"type\":[\"null\",\"long\"],\"default\":null},{\"name\":\"appeal_id\",\"type\":[\"null\",\"long\"],\"default\":null},{\"name\":\"goal_id\",\"type\":[\"null\",\"long\"],\"default\":null},{\"name\":\"cm_code\",\"type\":[\"null\",\"string\"],\"default\":null},{\"name\":\"promo_type\",\"type\":[\"null\",\"int\"],\"default\":null},{\"name\":\"platform\",\"type\":[\"null\",\"string\"],\"default\":null},{\"name\":\"device_model\",\"type\":[\"null\",\"string\"],\"default\":null},{\"name\":\"os_version\",\"type\":[\"null\",\"string\"],\"default\":null},{\"name\":\"app_version\",\"type\":[\"null\",\"string\"],\"default\":null},{\"name\":\"useragent\",\"type\":[\"null\",\"string\"],\"default\":null},{\"name\":\"url\",\"type\":[\"null\",\"string\"],\"default\":null},{\"name\":\"portrait\",\"type\":[\"null\",\"boolean\"],\"default\":null},{\"name\":\"position\",\"type\":[\"null\",\"int\"],\"default\":null}]}";
+/*    		
     	     "{\"namespace\": \"trackingad.avro\",\n"
               + " \"type\": \"record\",\n"
               + " \"name\": \"TrackingAd\",\n"
@@ -160,11 +140,15 @@ public class TestPubsubToGcs {
     		  + "       {\"name\":\"position\",\"type\":[\"null\",\"int\"],\"default\":null}"
               + " ]\n"
               + "}";     
-
+*/
     private static final Schema SCHEMA = new Schema.Parser().parse(SCHEMA_STRING);
     
     static class AvroDestination extends DynamicAvroDestinations<PubsubMessage,String,TrackingAdBQ>{
-    	private final String baseDir;
+    	/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+		private final String baseDir;
 
     	public AvroDestination(String baseDir) {
             System.out.println("AvroDestination");
@@ -187,12 +171,12 @@ public class TestPubsubToGcs {
                 System.out.println("formatRecord");
             	
             	PubsubMessage pubsubMessage = record;
-            	String json = new String(pubsubMessage.getPayload(), "UTF-8");
-                System.out.println("formatRecord:json:" + json);
-  //              AvroCoder<TrackingAd> coder = AvroCoder.of(TrackingAd.class);
-  //              trackingAd = CoderUtils.decodeFromByteArray(coder, pubsubMessage.getPayload());
-                ObjectMapper mapper = new ObjectMapper();
-                TrackingAd trackingAd = mapper.readValue(json, TrackingAd.class);
+//            	String json = new String(pubsubMessage.getPayload(), "UTF-8");
+//                System.out.println("formatRecord:json:" + json);
+//                ObjectMapper mapper = new ObjectMapper();
+//                TrackingAd trackingAd = mapper.readValue(json, TrackingAd.class);
+                AvroCoder<TrackingAd> coder = AvroCoder.of(TrackingAd.class);
+                TrackingAd trackingAd = CoderUtils.decodeFromByteArray(coder, pubsubMessage.getPayload());
                 trackingAdbq = TrackingAdMap.TrackingAdToTrackingAdBQ(trackingAd);
                 
                 System.out.println("formatRecord:trackingAdbq.uuid:" + trackingAdbq.uuid);
@@ -232,7 +216,8 @@ public class TestPubsubToGcs {
 
     }
 
-    public static void main(String[] args) throws IOException {
+    @SuppressWarnings("deprecation")
+	public static void main(String[] args) throws IOException {
         ReadDataOptions options = PipelineOptionsFactory.fromArgs(args).withValidation()
       .as(ReadDataOptions.class);
         
@@ -262,23 +247,26 @@ public class TestPubsubToGcs {
        //   		.withAllowedLateness(Duration.standardHours(1))
           		);
         
-  //      PCollection<PubsubMessage> PubsubtransformData = windowed_items		
-  //              .apply("ParseTrackingAd", ParDo.of(new ParseTrackingAdFn()));
-
-//        PCollection<KV<TrackingAd, String>> windowed_counts = PubsubtransformData.apply(
-//        		<String>perElement()
- //       		   Count.<String>perElement());
-        		 
-        windowed_items
+//        PCollection<TrackingAdBQ> PubsubtransformData = windowed_items		
+//                .apply("ParseTrackingAd", ParDo.of(new ParseTrackingAdFn()));
 /*
          .apply("WriteText", TextIO.<PubsubMessage>writeCustomType().to(new AvroDestination(options.getOutput()))
-//              .withSchema(schema)
               .withWindowedWrites()
               .withTempDirectory(StaticValueProvider.of(
                       FileSystems.matchNewResource(options.getOutput(), true)))
       		.withNumShards(options.getNumShards())
       		);
+
+        PubsubtransformData.apply("WriteAvros", AvroIO.write(TrackingAdBQ.class).to(options.getOutput())
+                .withSchema(SCHEMA)
+                .withWindowedWrites()
+                .withTempDirectory(StaticValueProvider.of(
+                        FileSystems.matchNewResource(options.getOutput(), true)))
+        		.withNumShards(options.getNumShards())
+        		);
 */
+
+        windowed_items
         .apply("WriteAvros", AvroIO.<PubsubMessage,TrackingAdBQ>writeCustomType().to(new AvroDestination(options.getOutput()))
                 .withSchema(SCHEMA)
                 .withWindowedWrites()
@@ -286,7 +274,7 @@ public class TestPubsubToGcs {
                         FileSystems.matchNewResource(options.getOutput(), true)))
         		.withNumShards(options.getNumShards())
         		);
-     
+    
         pipeline.run();
     }
 
